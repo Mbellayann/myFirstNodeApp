1c7ab37b76d3030775f8c2eb1e033b682931eff0
mbellaonguene <mbellaonguene@gmail.com>
2021-11-26 15:29:56 +0100

    adding in gitignore

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello nours ?");
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.status(200).send('<h1>Hello world</>')
+})
+
+app.listen(5000, () => {
+    console.log('server rruning on http://localhost:5000')
+})
